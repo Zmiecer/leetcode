5c0c41b2405d36544695b951de0e94eae456d8b1
Dzmitry Bylinovich <dzmitry.bylinovich@gmail.com>
2017-03-20 22:10:56 +0200

    Update 506.py

--- 506.py
+++ 506.py
@@ -4,18 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[str]
         """
-        def reverse_numeric(x, y):
+        def reverse(x, y):
             return y - x
-        kek = sorted(nums, cmp=reverse_numeric)
-        l = len(nums)
-        if l > 0:
-            nums[nums.index(kek[0])] = "Gold Medal"
-        if l > 1:
-            nums[nums.index(kek[1])] = "Silver Medal"
-        if l > 2:
-            nums[nums.index(kek[2])] = "Bronze Medal"
-        if l > 3:
-            for i in range(3, l):
-                nums[nums.index(kek[i])] = str(i + 1)
-        return nums
+
+        sort = sorted(nums, reverse)
+        rank = ["Gold Medal", "Silver Medal", "Bronze Medal"] + map(str, range(4, len(nums) + 1))
+        return map(dict(zip(sort, rank)).get, nums)
         
