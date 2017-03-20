bce7a07e867fee4947cf7c0b5ad8bafdf35221a1
Dzmitry Bylinovich <dzmitry.bylinovich@gmail.com>
2017-03-20 21:15:00 +0200

    Update 371.cpp

--- 371.cpp
+++ 371.cpp
@@ -3,12 +3,11 @@ class Solution
 public:
     int getSum(int a, int b) 
     {
-        while(b!=0)
+        while(b != 0)
         {
-            int c = a&b;
-            a=a^b;
-            b=c<<1;
-            
+            int c = a & b;
+            a = a ^ b;
+            b = c << 1;
         }
         return a;
         
