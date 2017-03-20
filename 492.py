b6dd5fbe9ef8a4457a3b7c617a7a64a3fb1fe813
Dzmitry Bylinovich <dzmitry.bylinovich@gmail.com>
2017-03-20 21:42:09 +0200

    Update 492.py

--- 492.py
+++ 492.py
@@ -1,15 +1,13 @@
-import math
 class Solution(object):
     def constructRectangle(self, area):
         """
         :type area: int
         :rtype: List[int]
         """
-        w = int(math.floor(math.sqrt(area)))
+        w = int(math.sqrt(area))
         while w > 1:
-            d = int(area % w)
-            if d == 0:
+            if area % w == 0:
                 return [int(area/w), w]
             w -= 1
-        return [area, w]
+        return [area, 1]
         
